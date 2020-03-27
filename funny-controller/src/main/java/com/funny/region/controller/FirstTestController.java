16b03fae6f7c83d79c4e5825dba388ba9775f239
rambo <476298823@qq.com>
2020-03-27 21:20:23 +0800

    测试提交

---
+++ funny-controller/src/main/java/com/funny/region/controller/FirstTestController.java
@@ -0,0 +1,7 @@
+package com.funny.region.controller;
+
+public class FirstTestController {
+    public static void main(String[] args) {
+        System.out.println("the first test demo");
+    }
+}
---
+++ funny-service/src/main/java/com/funny/region/service/FirstTestService.java
@@ -0,0 +1,10 @@
+package com.funny.region.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FirstTestService {
+
+
+
+}
